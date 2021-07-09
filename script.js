2589a5d067be69cc9e9da2a15fedf67143a55bc3
savannah-hayes <79738821+savannah-hayes@users.noreply.github.com>
2021-07-09 23:28:22 +0200

    Reset score on click of reset button

--- script.js
+++ script.js
@@ -3,28 +3,55 @@ const playerTwo = document.querySelector("#playerTwoScore");
 const playerOneButton = document.querySelector("#playerOneButton");
 const playerTwoButton = document.querySelector("#playerTwoButton");
 const resetButton = document.querySelector(".reset");
+
 let playerOneScore = 0;
 let playerTwoScore = 0;
 let winningScore = 5;
+let isGameOver = false;
 
 playerOneButton.addEventListener("click", () => {
-  playerOneScore += 1;
-  playerOne.textContent = playerOneScore;
-  if (playerOneScore === winningScore) {
-    playerOne.style.color = "green";
-    playerTwo.style.color = "red";
-    playerOneButton.disabled = true;
-    playerTwoButton.disabled = true;
+  if (!isGameOver) {
+    playerOneScore += 1;
+    if (playerOneScore === winningScore) {
+      isGameOver = true;
+      playerOne.style.color = "green";
+      playerTwo.style.color = "red";
+      playerOneButton.disabled = true;
+      playerTwoButton.disabled = true;
+    }
+    playerOne.textContent = playerOneScore;
   }
+
+  resetScoring();
 });
 
 playerTwoButton.addEventListener("click", () => {
-  playerTwoScore += 1;
-  playerTwo.textContent = playerTwoScore;
-  if (playerTwoScore === winningScore) {
-    playerTwo.style.color = "green";
-    playerOne.style.color = "red";
-    playerOneButton.disabled = true;
-    playerTwoButton.disabled = true;
+  if (!isGameOver) {
+    playerTwoScore += 1;
+    if (playerTwoScore === winningScore) {
+      isGameOver = true;
+      playerTwo.style.color = "green";
+      playerOne.style.color = "red";
+      playerOneButton.disabled = true;
+      playerTwoButton.disabled = true;
+    }
+    playerTwo.textContent = playerTwoScore;
   }
+
+  resetScoring();
 });
+
+function resetScoring() {
+  resetButton.addEventListener("click", () => {
+    playerOne.innerHTML = 0;
+    playerTwo.innerHTML = 0;
+    playerOne.style.color = "black";
+    playerTwo.style.color = "black";
+  });
+  resetButton.addEventListener("click", () => {
+    playerOne.innerHTML = 0;
+    playerTwo.innerHTML = 0;
+    playerOne.style.color = "black";
+    playerTwo.style.color = "black";
+  });
+}
